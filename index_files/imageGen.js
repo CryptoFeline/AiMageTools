530472f91be1bede8a3926d0ebb08ea36ae87c6a
CryptoFeline <90272853+CryptoFeline@users.noreply.github.com>
2023-12-18 15:30:51 +0100

    update

--- index_files/imageGen.js
+++ index_files/imageGen.js
@@ -7,16 +7,34 @@ function getImage() {
           
       const query = document.getElementById("queryTb").value;
 
-    fetch('https://hilltophse.co.uk/api/getImage2.php?query=' + query)
-      .then(response => response.json()) // Parse the JSON response
+      fetch('https://hilltophse.co.uk/api/getImage2.php?query=' + query)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
       .then(data => {
-          const imageUrl = data.image_url; // Extract the image URL
-          document.getElementById("preview").src = imageUrl;
-        document.getElementById("loadingLb").style.display = "none";
-        document.getElementById("queryTb").disabled = false;
-        document.getElementById("generateBtn").disabled = false;
-        document.getElementById("preview").style.display = "block";
-        });
+          if (data.image_url) {
+              document.getElementById("preview").src = data.image_url;
+              document.getElementById("loadingLb").style.display = "none";
+              document.getElementById("preview").style.display = "block";
+          } else {
+              throw new Error('Image URL not found in response');
+          }
+      })
+      .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+          // Handle the error, show an error message to the user
+          // Re-enable the inputs
+          document.getElementById("loadingLb").style.display = "none";
+          document.getElementById("queryTb").disabled = false;
+          document.getElementById("generateBtn").disabled = false;
+          
+          // Show an error section or message
+          document.getElementById('errorSection').style.display = 'block';
+          document.getElementById('errorMessage').textContent = 'Error fetching image: ' + error.message;
+      });
     };
 
 function displayGeneratedImage(imageUrl) {
@@ -30,3 +48,23 @@ function displayGeneratedImage(imageUrl) {
     // Remove the background logo and the semi-transparent black background from the previewBox
     previewBox.style.backgroundImage = 'none';
 }
+
+function downloadImage() {
+  const image = document.getElementById('preview');
+  if (image && image.src) {
+      const downloadLink = document.createElement('a');
+      downloadLink.href = image.src;
+
+      const imageName = image.src.split('/').pop().split('?')[0];
+      downloadLink.download = imageName;
+      
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+  } else {
+      alert("No image available for download.");
+  }
+}
+
+// Attach this function to the "Save" button
+document.getElementById('saveImage').addEventListener('click', downloadImage);
