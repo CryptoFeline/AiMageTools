1e7e07d22071f798ee5912f491cf47cbcc257ff2
CryptoFeline <90272853+CryptoFeline@users.noreply.github.com>
2023-12-18 14:32:32 +0100

    update

--- index_files/connect.js
+++ index_files/connect.js
@@ -1,4 +1,4 @@
-import Web3 from 'web3';
+import Web3 from 'https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js';
 
 async function connectWallet() {
     if (window.ethereum) { // Check if MetaMask is installed
