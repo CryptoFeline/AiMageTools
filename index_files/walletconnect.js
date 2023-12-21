74db811fa037257d7f7da1bbb6e52e7173bb5724
CryptoFeline <90272853+CryptoFeline@users.noreply.github.com>
2023-12-21 16:09:04 +0100

    up

--- index_files/walletconnect.js
+++ index_files/walletconnect.js
@@ -1,24 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const projectId = '425e9e4db7894509a4dc5721e55b0aca';
-    if (!projectId) {
-      throw new Error('VITE_PROJECT_ID is not set')
-    }
+    const projectId = '425e9e4db7894509a4dc5721e55b0aca'; // Your Infura Project ID
   
-    var provider = new WalletConnectProvider.default({
-      infuraId: projectId, // Use the projectId as the infuraId
-      rpc: {1: "https://mainnet.infura.io/v3/" + projectId} // Mainnet RPC URL
+    // Web3Modal instance
+    const web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: false, // optional
+      providerOptions: {
+        walletconnect: {
+          package: WalletConnectProvider, // required
+          options: {
+            infuraId: projectId, // required
+          },
+        },
+      },
     });
   
     async function connectWallet() {
       try {
         // Open the connect modal
-        const accounts = await provider.enable();
-  
-        // Connection is successful
-        console.log("Wallet connected:", accounts);
+        const provider = await web3Modal.connect();
   
         // Create web3 instance
         const web3 = new Web3(provider);
+  
+        // Get connected accounts
+        const accounts = await web3.eth.getAccounts();
+        console.log("Wallet connected:", accounts);  
   
         const tokenContractAddress = '0x4872208C83acBfd7f6Dea5AA6CE6d5D7aED2AC1C';
         const userAddress = accounts[0];
@@ -48,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function() {
           document.getElementById('errorMessage').style.display = 'block';
         }
   
-      } catch (error) {
+    } catch (error) {
         console.error("Could not connect to wallet:", error);
         document.getElementById('errorMessage').innerHTML = "Error connecting to wallet: " + error.message;
         document.getElementById('errorMessage').style.display = 'block';
@@ -57,4 +64,4 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Event listeners
     document.getElementById('open-connect-modal').addEventListener('click', connectWallet);
-  });  
+  });
